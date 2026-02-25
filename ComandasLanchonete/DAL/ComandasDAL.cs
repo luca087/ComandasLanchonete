81518ae5e04cd2ea51c1b99c19d4882fa379123c
Lucas Lewin Vilbert <lucas.vilbert@remotatec.com.br>
2026-02-25 13:56:29 -0300

    comanda DAL 1

--- ComandasLanchonete/DAL/ComandasDAL.cs
+++ ComandasLanchonete/DAL/ComandasDAL.cs
@@ -1,5 +1,4 @@
 ﻿using ComandasLanchonete.DAL.Models;
-using ComandasLanchonete.Models;
 
 namespace ComandasLanchonete.DAL
 {
@@ -17,21 +16,37 @@ namespace ComandasLanchonete.DAL
             _context.Comandas.Add(comanda);
             _context.SaveChanges();
             return comanda;
-        } 
+        }
 
         public IEnumerable<ComandaDALModel> GetComandas()
         {
             return _context.Comandas;
         }
 
+        public ComandaDALModel GetComandaById(int id)
+        {
+            return _context.Comandas.Find(id);
+        }
+
         public void UpdateComanda(ComandaDALModel comanda)
         {
-            
+            _context.Comandas.Update(comanda);
+            _context.SaveChanges();
         }
 
         public void DeleteComanda(int id)
         {
-            
+            var comanda = GetComandaById(id);
+
+            if (comanda != null)
+            {
+                _context.Comandas.Remove(comanda);
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("Comanda não existente!");
+            }
         }
     }
 }
